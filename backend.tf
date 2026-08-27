ce449bce13c01d22ce99556eb2855f5f95ce2b7f
Sandeep Chenna Kumar <sandeep.chenna@ibm.com>
2026-08-27 21:27:57 +0530

    just having fun

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "skchenna-tflab-state-220828-sat"
-        key = "/gh-lab/state/test/test.tfstate"
+        key = "gh-lab/state/test/test.tfstate"
         use_lockfile = true
     }
 }
