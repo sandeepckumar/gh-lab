b83816962aba9ef812d4071cfdd6a2227b236bad
Sandeep Chenna Kumar <sandeep.chenna@ibm.com>
2026-08-27 12:35:24 +0530

    testing [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import MainContent from './components/MainContent';
-import logo from './assets/images/logo.png';
+import MainContent from "./components/MainContent";
+import logo from "./assets/images/logo.png";
+
+// testing
 
 function App() {
   return (
